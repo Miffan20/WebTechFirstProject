e557a5b7d67072660193b2f0223ba3a3b08fba0a
Christoffer Rosenkilde <Crose19@student.sdu.dk>
2022-10-09 21:57:16 +0200

    Small changes made to the code, and possible fixed the color mismatch to pass final test

--- public/js/add-to-cart.js
+++ public/js/add-to-cart.js
@@ -6,25 +6,18 @@ function incrementValue(){
     document.getElementById('cart-count').innerHTML = count;
 }
 
-
 function plebBoiButtonController(){
-
-
-
     var x = document.getElementById("color").value;
-
     if (x === "red" ||x === "blue" || x === "green") {
-
         incrementValue();
-    }
-    else {
+    } else {
         document.getElementById("add-to-cart").disabled = true;
         alert("choose color")
     }
 }
 
-
 const selectElement = document.querySelector('#color');
+
 
 selectElement.addEventListener('change', (event) => {
 
@@ -33,13 +26,11 @@ selectElement.addEventListener('change', (event) => {
     document.querySelector('#pc-image').src = properSrc;
 });
 
-
 selectElement.addEventListener('change', (event) => {
-
     let properSrc = location.origin + `/img/plebboi/${event.target.value}.png`;
+
     if (event.target.value === "red" || event.target.value === "green" ||event.target.value === "blue" ){
         document.getElementById("add-to-cart").disabled = false;
     }
-
 });
 
